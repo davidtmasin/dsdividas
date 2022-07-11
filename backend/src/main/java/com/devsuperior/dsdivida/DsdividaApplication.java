c243799633773c7fe7a94d7757dc4056d1a6309f
David Teixeira de Masin <dtmasin@sfiec.org.br>
2022-07-11 08:55:52 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdivida/DsdividaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdivida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdividaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdividaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdivida/DsdividaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdivida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdividaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
