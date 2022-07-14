1349349d1b45862eb97792eabe488bb5aee094a9
David Teixeira de Masin <dtmasin@sfiec.org.br>
2022-07-14 17:39:22 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsdivida/controllers/DebtController.java
+++ backend/src/main/java/com/devsuperior/dsdivida/controllers/DebtController.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsdivida.entities.Debt;
 import com.devsuperior.dsdivida.services.DebtService;
+import com.devsuperior.dsdivida.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/debts")
 public class DebtController {
 
 	@Autowired
-	private DebtService debtServ;	
+	private DebtService debtServ;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Debt> findDebts(
@@ -24,6 +29,11 @@ public class DebtController {
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
 			Pageable pageable){
 		return debtServ.findDebts(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 	
 }
---
+++ backend/src/main/java/com/devsuperior/dsdivida/services/SmsService.java
@@ -0,0 +1,57 @@
+package com.devsuperior.dsdivida.services;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsdivida.entities.Debt;
+import com.devsuperior.dsdivida.repositories.DebtRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private DebtRepository debtRepository;
+	
+	public void sendSms(Long debtId) {
+		
+		Debt debt = debtRepository.findById(debtId).get();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+//		String date = debt.getDate().getDayOfMonth() + "/" + debt.getDate().getMonthValue() + "/" + debt.getDate().getYear(); 
+		String date = (debt.getDate().getDayOfMonth() < 10 ? ("0" + debt.getDate().getDayOfMonth()) : (debt.getDate().getDayOfMonth()) ) + "/" 
+						+ (debt.getDate().getMonthValue() < 10 ? ("0" + debt.getDate().getMonthValue()) : (debt.getDate().getMonthValue()) ) + "/" 
+						+ debt.getDate().getYear(); 		
+				
+		String msg = "Pagamento efetuado com sucesso!\n" + "Segue abaixo, o demonstrativo:\n" 
+				   + "Descrição: " + debt.getDescription() + ".\n" 
+		           + "Valor: R$ " + String.format("%.2f",debt.getAmount()) + ".\n" 
+				   + "Tipo de despesa: " + debt.getExpenseType() + ".\n" 
+				   + "Data do pagamento: " + date + ".";
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println("Mensagem enviada com sucesso! ID: " + message.getSid());
+	}
+}
